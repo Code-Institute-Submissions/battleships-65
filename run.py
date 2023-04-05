1cbf6bb5df4ac9127ab98a2fe5defd659f765f15
louiseconlon11 <louiseconlon11@gmail.com>
2023-04-05 15:37:02 +0000

    Convert user choice of grid to integer

--- run.py
+++ run.py
@@ -1,8 +1,16 @@
 def main():
     print("Welcome to Battleships\n")
-    print(input("Please enter your name: \n"))
+    input("Please enter your name: \n")
     print("Please choose which grid you wish to play on\n")
     print("Grid sizes are 5, 8 or 10\n")
-    print(int(input("Grid size choice:")))
+    user_choice = int(input("Grid size choice:"))
+    validate_grid_size(user_choice)
+
+def validate_grid_size(choice):
+    try:
+        if choice != 5 or 8 or 10:
+            raise ValueError(f"Error. You chose {choice} Only values of 5, 8 and 10 are accepted\n")
+    except ValueError as e:
+        print(f"You have chosen {choice}. This is invalid. Please try again\n")
 
 main()
