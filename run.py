2833993ba028ed65a975445d05521c22f7eec2ba
louiseconlon11 <louiseconlon11@gmail.com>
2023-04-05 15:01:59 +0000

    Code welcome message and user input for name and grid size

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def main():
+    print("Welcome to Battleships\n")
+    print(input("Please enter your name: \n"))
+    print("Please choose which grid you wish to play on\n")
+    print("Grid sizes are 5, 8 or 10\n")
+    print(int(input("Grid size choice:")))
+
+main()
