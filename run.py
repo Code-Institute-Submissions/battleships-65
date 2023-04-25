2dba7bbc4502e9b529cd9596fa0eed8b26d5814c
louiseconlon11 <louiseconlon11@gmail.com>
2023-04-25 13:11:23 +0000

    Code to call display board function within choice validation

--- run.py
+++ run.py
@@ -1,26 +1,25 @@
-# def main():
-#     print("Welcome to Battleships\n")
-#     input("Please enter your name: \n")
-#     print("Please choose which grid you wish to play on\n")
-#     print("Grid sizes are 5, 8 or 10\n")
-#     user_choice = int(input("Grid size choice: "))
-#     validate_grid_size(user_choice)
+def main():
+    print("Welcome to Battleships\n")
+    input("Please enter your name: \n")
+    print("Please choose which grid you wish to play on\n")
+    print("Grid sizes are 5, 8 or 10\n")
+    user_choice = int(input("Grid size choice: "))
+    validate_grid_size(user_choice)
 
 
-# def validate_grid_size(choice):
-#     try:
-#         if choice not in [5, 8, 10]:
-#             print(f"Choice of {choice} is invalid, please choose a valid choice\n")
-#         else:
-#             confirm = input(f"please confirm choice of {choice}, press Y to confirm ")
-#             if confirm in ["y", "Y", "Yes", "YES", "yes"]:
-#                 print("Thank you, the game will now commence")
-#                 # game logic
-#             display_board(game_board)
-#             else:
-#                 main()
-#     except ValueError as e:
-#         print(f"{e} This choice is invalid. Please try again\n")
+def validate_grid_size(choice):
+    try:
+        if choice not in [5, 8, 10]:
+            print(f"Choice of {choice} is invalid, please choose a valid choice\n")
+        else:
+            confirm = input(f"please confirm choice of {choice}, press Y to confirm ")
+            if confirm in ["y", "Y", "Yes", "YES", "yes"]:
+                print("Thank you, the game will now commence")
+                display_board(game_board)
+            else:
+                main()
+    except ValueError as e:
+        print(f"{e} This choice is invalid. Please try again\n")
 
 
 def display_board(game_board):
@@ -34,7 +33,7 @@ def display_board(game_board):
             print(game_board[letter][column], end=" ")
         letter += 1
         print("| ")
-game_board = []
-display_board(game_board)
-# main()
 
+
+game_board = []
+main()
